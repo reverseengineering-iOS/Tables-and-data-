bfa5bd4ea676b3d7cece82817740e1f1788dd6b7
sai <saivishal.ios@gmail.com>
2018-03-06 11:27:37 -0500

    1) Added new view controller, Added new colletion view cell, 2) Added IBOutlets forthe collection view cell

---
+++ Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Saivishal Gummadi on 3/6/18.
+//  Copyright © 2018 ReverseEngineering. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
